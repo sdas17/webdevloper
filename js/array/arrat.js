ca0d42934e0a555e9ac847c43648874f3fc2105c
sdas17 <subhamkvgms112@gmail.com>
2025-12-07 22:43:10 +0530

    array in js

---
+++ js/array/arrat.js
@@ -0,0 +1,24 @@
+const array_sample=['subham',true,false,12,23,44,66]
+// console.log(array_sample[0])//subham
+// console.log(array_sample[-1])
+// console.log(array_sample.at(-1))
+// const data =structuredClone(array_sample)
+
+// // console.log(data== array_sample)
+// array_sample.push("subham kumar das")
+// console.log(array_sample)
+// // console.log(array_sample.pop(),array_sample)
+// array_sample.unshift('rahul')
+// console.log(array_sample)
+// array_sample.shift()
+// console.table(array_sample)
+// delete array_sample[0]
+// console.table(array_sample)
+// console.table(array_sample)
+// let sampledata=["sam","subham",'kumar','sda',"pledata"]
+// // console.table(array_sample.splice(2,4))
+// let data =array_sample.push(sampledata)
+// console.log(array_sample.flat(Infinity))
+// let sample = new Array("sam","subham",'kumar','sda',"pledata")
+// console.log(typeof sample);
+
