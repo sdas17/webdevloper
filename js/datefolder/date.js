14b59b4386ae8b70dfd6695f46251c49b93c0b86
sdas17 <subhamkvgms112@gmail.com>
2025-12-08 08:05:04 +0530

    date in js -subham

---
+++ js/datefolder/date.js
@@ -0,0 +1,22 @@
+// const date_filter=new Date(1765129567524
+// )
+// // console.log(date_filter.toDateString());
+// // console.log(date_filter.toString())
+// // console.log(date_filter.toISOString());
+// console.log(date_filter.getDate());
+// console.log(date_filter.getDay()+1)
+// console.log(date_filter.getMonth()+1)
+// console.log(date_filter.getFullYear());
+// console.log(date_filter.getTime());
+// const date_now=Date.now()
+// console.log(date_now-date_filter.getTime())
+// const date_filters=new Date(2010,11,21)
+// console.log(date_filters)
+// const date_filters=new Date()
+// const date_range=new Date("2026-06-23T00:00:00")
+// const diff=date_filters-date_range
+// console.log(Math.floor(diff/(1000*60*60*24))%24)
+//  1000 1sec 1 ms
+// 1 minute 60 sec
+// 1 sec 60 minute
+// 1 day 24 hour 
---
+++ js/object/index.js
@@ -0,0 +1,1 @@
+// object conectp starty
