3983222081fd18185e7743d86a09d2b1fad66a46
sdas17 <subhamkvgms112@gmail.com>
2025-12-08 23:41:23 +0530

    string practise -subham

---
+++ js/practise/string.js
@@ -0,0 +1,19 @@
+let sample =' subhams kumar             das'
+console.log(sample.length);
+// console.log(sample.charAt(0));
+// console.log(sample.includes("s"));
+// console.log(sample.indexOf("s"));
+// console.log(sample.lastIndexOf("s"));
+// console.log(sample.slice(0,-2));
+// console.log(sample.substring(0,-2));
+// console.log(sample.split("").join(""));
+let result = sample.trim();
+console.log(result.length,'11');
+
+
+
+
+
+
+
+
