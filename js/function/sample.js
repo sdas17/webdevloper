e98e0d6891a2d42cc154ae0ac73867c6413eb992
sdas17 <subhamkvgms112@gmail.com>
2025-12-14 22:20:56 +0530

    set.js -subham

---
+++ js/function/sample.js
@@ -0,0 +1,78 @@
+// const arr = [10, 20, 30];
+
+// arr.forEach((data,index,arr)=>{
+//     arr[index]=arr[index]*2
+// })
+// console.log(arr);
+
+
+// arr.forEach((arrdata,index,data)=>{
+//    arr[index]=data[index]*2
+   
+   
+    
+// })
+// console.log(arr);
+// const arr = [10, 21, 30, 41, 50];
+
+
+// arr.forEach((value, index) => {
+//   if (value % 2 !== 0) {
+//     arr[index] = 0;
+//   }
+// });
+
+// console.log(arr);
+
+
+// let fruits = [
+//   "Apple",
+//   "Banana",
+//   "Orange",
+//     "Apple",
+//   "Banana",
+//   "Orange",
+//     "Apple",
+//   "Banana",
+//   "Orange",
+//     "Apple",
+//   "Banana",
+//   "Orange",
+//     "Apple",
+//   "Banana",
+//   "Orange",
+//   "Apple",
+//   "Banana",
+//   "Orange",
+//     "Apple",
+//   "Banana",
+//   "Orange",
+//     "Apple",
+//   "Banana",
+//   "Orange",
+//     "Apple",
+//   "Banana",
+//   "Orange",
+//     "Apple",
+//   "Banana",
+//   "Orange",
+//   "Mango",
+//   "Grapes",
+//   "Pineapple",
+//   "Strawberry",
+//   "Watermelon",
+//   "Papaya",
+//   "Kiwi",
+//   "Guava",
+//   "Pomegranate"
+// ];
+// const data = fruits.reduce((curr, item) => {
+
+//   curr.hasOwnProperty(item) ?curr[item]++:    curr[item] = 1;
+//   return curr; 
+// }, {});
+// const {Banana,Orange}=data
+// console.log(Banana,Orange);
+
+
+
