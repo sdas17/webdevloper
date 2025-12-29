2003983f3ddb7cfbc9f69f6a7a143e49f176f49c
sdas17 <subhamkvgms112@gmail.com>
2025-12-29 23:09:09 +0530

    project md

---
+++ js/project/din-out.js
@@ -0,0 +1,38 @@
+// dumy data 
+
+// resturant_information
+// Name
+// Rating
+// food_type
+// price_for_two
+// location
+// distance
+// offers
+// alchol
+// restaurant_opening_time
+// restaurant_close_time
+const names = [
+  "Spice Garden", "Ocean Delight", "Urban Tadka", "Green Bowl",
+  "Night Owl Cafe", "Royal Feast", "Food Hub", "Taste of India",
+  "Flavour Town", "Cafe Bliss"
+];
+const foodTypes = [
+  "North Indian", "South Indian", "Chinese", "Italian",
+  "Continental", "Punjabi", "Mughlai", "Fast Food", "Cafe", "Seafood"
+];
+
+const locations = [
+  "Andheri", "Bandra", "Powai", "Lower Parel",
+  "Borivali", "Malad", "Goregaon", "Kurla", "Thane", "Vashi"
+];
+
+const offersList = [
+  "20% OFF",
+  "Flat ₹200 OFF",
+  "Buy 1 Get 1 Free",
+  "Free Dessert",
+  "No Offer"
+];
+// Random price = Math.random() × range + minimum
+// Math.floor(Math.random() * (max - min + 1)) + min
+
---
+++ js/project/ecom.md
@@ -0,0 +1,5 @@
+# project ecommerce 2 days
+# 2 days gits 
+# 2 days typescript
+# react js
+# backend
