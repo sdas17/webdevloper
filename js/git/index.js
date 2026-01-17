c3213f254da554fda2b30ffd07324cc04d37b09b
sdas17 <subhamkvgms112@gmail.com>
2026-01-17 19:20:32 +0530

    Revert "secound-commit"
    This reverts commit ad2d965f80cecd3b5cfafe6bbb79231c919224b2.

--- js/git/index.js
+++ js/git/index.js
@@ -1,9 +1,9 @@
 const name='subham'
 const age =200
 const balance =300
-const  account=203
+// const  account=203
 function update_balance(){
     balance=200+200
 }
 update_balance()
-console.log('hii my name is subham')
+console.log('hii my name is')
